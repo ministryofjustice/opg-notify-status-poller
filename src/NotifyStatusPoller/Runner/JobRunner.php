81c00b61519f5636220d9c76d8cd9238685ea1c5
Nick Pyett <nick.pyett@digital.justice.gov.uk>
2024-08-12 12:30:10 +0100

    Add counts of NotifyStatus response from Gov Notify to logging

--- src/NotifyStatusPoller/Runner/JobRunner.php
+++ src/NotifyStatusPoller/Runner/JobRunner.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace NotifyStatusPoller\Runner;
 
 use NotifyStatusPoller\Exception\NotificationNotFoundException;
+use NotifyStatusPoller\Mapper\NotifyStatus;
 use Throwable;
 use Psr\Log\LoggerInterface;
 use NotifyStatusPoller\Command\Handler\UpdateDocumentStatusHandler;
@@ -60,13 +61,34 @@ class JobRunner
         }
 
         $this->logger->info('Updating', ['count' => count($inProgressResults), 'context' => Context::NOTIFY_POLLER]);
+
         $updatedCount = 0;
+        $notifyStatusCounts = [
+            NotifyStatus::PENDING_VIRUS_CHECK => 0,
+            NotifyStatus::VIRUS_SCAN_FAILED => 0,
+            NotifyStatus::VALIDATION_FAILED => 0,
+            NotifyStatus::FAILED => 0,
+            NotifyStatus::ACCEPTED => 0,
+            NotifyStatus::RECEIVED => 0,
+            NotifyStatus::CANCELLED => 0,
+            NotifyStatus::TECHNICAL_FAILURE => 0,
+            NotifyStatus::PERMANENT_FAILURE => 0,
+            NotifyStatus::TEMPORARY_FAILURE => 0,
+            NotifyStatus::CREATED => 0,
+            NotifyStatus::SENDING => 0,
+            NotifyStatus::DELIVERED => 0,
+        ];
 
         foreach ($inProgressResults as $getNotifyStatus) {
             try {
                 $updateDocumentStatus = $this->getNotifyStatusHandler->handle($getNotifyStatus);
                 $this->updateDocumentStatusHandler->handle($updateDocumentStatus);
+
                 $updatedCount++;
+
+                if (array_key_exists($updateDocumentStatus->getNotifyStatus(), $notifyStatusCounts)) {
+                    $notifyStatusCounts[$updateDocumentStatus->getNotifyStatus()]++;
+                }
             } catch (NotificationNotFoundException $e) {
                 $this->logger
                     ->info(
@@ -82,6 +104,6 @@ class JobRunner
             }
         }
 
-        $this->logger->info('Finished', ['count' => $updatedCount, 'context' => Context::NOTIFY_POLLER]);
+        $this->logger->info('Finished', ['count' => $updatedCount, 'notify_status_counts' => $notifyStatusCounts, 'context' => Context::NOTIFY_POLLER]);
     }
 }
